224f7d1765c336cd8715dddf4c1787a09434e16f
senaiapy <senaia.py@gmail.com>
2024-01-25 18:54:30 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Clock from "@/components/clock-counter/clock";
+import Counter from "@/components/clock-counter/counter";
+
+export default function Page() {
+  return (
+    <>
+      <Clock />
+      <Counter />
+    </>
+  );
+}
